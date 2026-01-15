afbda7cb5579f7b993708c2002682bab2a2e4b5a
AyushSolves <ayushnegi196@gmail.com>
2026-01-15 12:04:38 +0530

    Add files via upload

--- Solution.java
+++ Solution.java
@@ -1,20 +1,22 @@
 class Solution {
-    public int removeDuplicates(int[] nums) 
-    {
-       
-        if (nums.length == 0) return 0;
+    public int strStr(String haystack, String needle) {
+        int n = haystack.length();
+        int m = needle.length();
 
-        int k = 1;
+        if (m > n) return -1;
 
-        for (int i = 1; i < nums.length; i++) 
-        {
-            if (nums[i] != nums[i - 1]) 
-            {
-                nums[k] = nums[i];
-                k++;
+        for (int i = 0; i <= n - m; i++) {
+            int j = 0;
+
+            while (j < m && haystack.charAt(i + j) == needle.charAt(j)) {
+                j++;
+            }
+
+            if (j == m) {
+                return i;
             }
         }
 
-        return k;
+        return -1;
     }
 }
