0a9fc370dce7e7b744008e50a06e14a733cbe9fe
AyushSolves <ayushnegi196@gmail.com>
2026-01-16 10:39:51 +0530

    Add files via upload

--- Solution.java
+++ Solution.java
@@ -1,22 +1,22 @@
 class Solution {
-    public int strStr(String haystack, String needle) {
-        int n = haystack.length();
-        int m = needle.length();
+    public int searchInsert(int[] nums, int target) {
+        int low = 0;
+        int high = nums.length - 1;
 
-        if (m > n) return -1;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
 
-        for (int i = 0; i <= n - m; i++) {
-            int j = 0;
-
-            while (j < m && haystack.charAt(i + j) == needle.charAt(j)) {
-                j++;
-            }
-
-            if (j == m) {
-                return i;
+            if (nums[mid] == target) {
+                return mid;  
+            } 
+            else if (nums[mid] < target) {
+                low = mid + 1;
+            } 
+            else {
+                high = mid - 1;
             }
         }
 
-        return -1;
+        return low;
     }
 }
