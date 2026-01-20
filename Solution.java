337d4ed90636347941867d0ba1035090bd26f760
AyushSolves <ayushnegi196@gmail.com>
2026-01-20 08:09:50 +0530

    Add files via upload

--- Solution.java
+++ Solution.java
@@ -1,30 +1,51 @@
-import java.util.Arrays;
+import java.util.Scanner;
 
-class Solution {
-
-    public int[] plusOne(int[] digits) {
-
-        for (int i = digits.length - 1; i >= 0; i--) {
-
-            if (digits[i] < 9) {
-                digits[i]++;
-                return digits;
-            }
-
-            digits[i] = 0;
+class Solution 
+{
+    public int mySqrt(int x) 
+    {
+        if (x == 0 || x == 1) 
+        {
+            return x;
         }
 
-        int[] result = new int[digits.length + 1];
-        result[0] = 1;
-        return result;
+        int low = 1, high = x;
+        int ans = 0;
+        
+        while (low <= high) 
+        {
+            int mid = low + (high - low) / 2;
+            long square = (long) mid * mid;   
+
+            if (square == x) 
+            {
+                return mid;
+            } 
+            else if (square < x) 
+            {
+                ans = mid;        
+                low = mid + 1;  
+            } 
+            else 
+            {
+                high = mid - 1;
+            }
+        }
+        return ans;  
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) 
+    {
+        Scanner sc = new Scanner(System.in);
         Solution sol = new Solution();
 
-        int[] digits = {9, 9, 9};   // Test input
-        int[] answer = sol.plusOne(digits);
+        System.out.print("Enter a non-negative number: ");
+        int x = sc.nextInt();
 
-        System.out.println(Arrays.toString(answer));
+        int result = sol.mySqrt(x);
+
+        System.out.println("Square root (rounded down) = " + result);
+
+        sc.close();
     }
 }
