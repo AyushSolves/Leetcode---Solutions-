a48dd911a3aaf1261030dbf10f3aa8ff94e71fcc
AyushSolves <ayushnegi196@gmail.com>
2026-01-24 21:55:07 +0530

    Add files via upload

--- Solution.java
+++ Solution.java
@@ -1,52 +1,19 @@
-class ListNode {
-    int val;
-    ListNode next;
-
-    ListNode(int val) {
-        this.val = val;
-        this.next = null;
-    }
-}
-
-class Solution 
-{
-    public ListNode deleteDuplicates(ListNode head) 
-    {
-        if (head == null || head.next == null) {
-            return head;
-        }
-
-        ListNode current = head;
-
-        while (current != null && current.next != null) {
-            if (current.val == current.next.val) 
-            {
-
-                current.next = current.next.next;
-            } 
-            else 
-            {
-                current = current.next;
-            }
-        }
-
-        return head;
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        inorder(root, result);
+        return result;
     }
 
-    public static void main(String[] args) {
-
-        ListNode head = new ListNode(1);
-        head.next = new ListNode(1);
-        head.next.next = new ListNode(2);
-        head.next.next.next = new ListNode(3);
-        head.next.next.next.next = new ListNode(3);
-
-        Solution sol = new Solution();
-        ListNode result = sol.deleteDuplicates(head);
-
-        while (result != null) {
-            System.out.print(result.val + " ");
-            result = result.next;
+    private void inorder(TreeNode node, List<Integer> result) {
+        if (node == null) {
+            return;
         }
+
+        inorder(node.left, result);
+
+        result.add(node.val);
+
+        inorder(node.right, result);
     }
 }
