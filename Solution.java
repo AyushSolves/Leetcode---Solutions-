427d1ae3d333ef8539f7467aa71cab4239ae8e68
AyushSolves <ayushnegi196@gmail.com>
2026-01-26 13:38:19 +0530

    Add files via upload

--- Solution.java
+++ Solution.java
@@ -1,19 +1,48 @@
-class Solution {
-    public List<Integer> inorderTraversal(TreeNode root) {
-        List<Integer> result = new ArrayList<>();
-        inorder(root, result);
-        return result;
+class TreeNode 
+{
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int val) 
+    {
+        this.val = val;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+class Solution 
+{
+    public boolean isSymmetric(TreeNode root) 
+    {
+        if (root == null) return true;
+        return isMirror(root.left, root.right);
     }
 
-    private void inorder(TreeNode node, List<Integer> result) {
-        if (node == null) {
-            return;
-        }
+    private boolean isMirror(TreeNode t1, TreeNode t2) 
+    {
+        if (t1 == null && t2 == null) return true;
+        if (t1 == null || t2 == null) return false;
 
-        inorder(node.left, result);
+        return (t1.val == t2.val)
+                && isMirror(t1.left, t2.right)
+                && isMirror(t1.right, t2.left);
+    }
 
-        result.add(node.val);
+    public static void main(String[] args) 
+    {
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(2);
 
-        inorder(node.right, result);
+        root.left.left = new TreeNode(3);
+        root.left.right = new TreeNode(4);
+
+        root.right.left = new TreeNode(4);
+        root.right.right = new TreeNode(3);
+
+        Solution sol = new Solution();
+        System.out.println(sol.isSymmetric(root));
     }
 }
