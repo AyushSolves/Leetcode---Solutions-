4edb6e22e843f589e533bb49eb70ba77cc42665c
AyushSolves <ayushnegi196@gmail.com>
2026-02-15 21:21:14 +0530

    Add files via upload

---
+++ isomorphicstring.py
@@ -0,0 +1,35 @@
+class Solution:
+    def isIsomorphic(self, s: str, t: str) -> bool:
+
+        mapST = {}
+        mapTS = {}
+
+        for i in range(len(s)):
+
+            c1 = s[i]
+            c2 = t[i]
+
+            if c1 in mapST:
+                if mapST[c1] != c2:
+                    return False
+            else:
+                mapST[c1] = c2
+
+            if c2 in mapTS:
+                if mapTS[c2] != c1:
+                    return False
+            else:
+                mapTS[c2] = c1
+
+        return True
+
+if __name__ == "__main__":
+
+    s = "egg"
+    t = "add"
+
+    sol = Solution()
+
+    print("s =", s)
+    print("t =", t)
+    print("Is Isomorphic:", sol.isIsomorphic(s, t))
