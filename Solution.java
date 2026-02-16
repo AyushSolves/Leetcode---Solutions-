89493601607ec3522a87669ffe8449539907dc0e
AyushSolves <ayushnegi196@gmail.com>
2026-02-16 10:22:19 +0530

    Add files via upload

--- Solution.java
+++ Solution.java
@@ -1,51 +1,57 @@
-class ListNode {
+class ListNode 
+{
     int val;
     ListNode next;
 
     ListNode() {}
 
-    ListNode(int val) {
+    ListNode(int val) 
+    {
         this.val = val;
     }
 
-    ListNode(int val, ListNode next) {
+    ListNode(int val, ListNode next) 
+    {
         this.val = val;
         this.next = next;
     }
 }
 
-public class Solution {
+public class Solution 
+{
 
-    public ListNode removeElements(ListNode head, int val) {
+    public ListNode reverseList(ListNode head) 
+    {
 
-        ListNode dummy = new ListNode(0);
-        dummy.next = head;
+        ListNode prev = null;
+        ListNode curr = head;
 
-        ListNode current = dummy;
+        while (curr != null) 
+        {
 
-        while (current.next != null) {
-            if (current.next.val == val) {
-                current.next = current.next.next;
-            } else {
-                current = current.next;
-            }
+            ListNode nextTemp = curr.next;
+
+            curr.next = prev; 
+
+            prev = curr; 
+            curr = nextTemp; 
         }
 
-        return dummy.next;
+        return prev;
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) 
+    {
 
         ListNode head = new ListNode(1,
                 new ListNode(2,
-                new ListNode(6,
                 new ListNode(3,
                 new ListNode(4,
-                new ListNode(5,
-                new ListNode(6)))))));
+                new ListNode(5)))));
 
         Solution sol = new Solution();
-        head = sol.removeElements(head, 6);
+
+        head = sol.reverseList(head);
 
         while (head != null) {
             System.out.print(head.val + " ");
