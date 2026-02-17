9646681c563b858e0c6ab150c8e5a691bbe608fb
AyushSolves <ayushnegi196@gmail.com>
2026-02-17 09:05:14 +0530

    Add files via upload

---
+++ containsduplicate.py
@@ -0,0 +1,19 @@
+class Solution:
+    def containsDuplicate(self, nums):
+        seen = set()  
+
+        for num in nums:
+            if num in seen:
+                return True 
+            seen.add(num)
+
+        return False   
+
+if __name__ == "__main__":
+
+    nums = [1, 2, 3, 1]  
+
+    sol = Solution()
+
+    print("Array:", nums)
+    print("Contains Duplicate:", sol.containsDuplicate(nums))
