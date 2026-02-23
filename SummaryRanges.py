0bc5ccc96a34785d251576b5133ef8197c848222
AyushSolves <ayushnegi196@gmail.com>
2026-02-23 09:45:34 +0530

    Add files via upload

---
+++ SummaryRanges.py
@@ -0,0 +1,31 @@
+class Solution:
+    def summaryRanges(self, nums):
+        result = []
+
+        if not nums:
+            return result
+
+        start = nums[0]
+
+        for i in range(1, len(nums)):
+
+            if nums[i] != nums[i - 1] + 1:
+
+                if start == nums[i - 1]:
+                    result.append(str(start))
+                else:
+                    result.append(str(start) + "->" + str(nums[i - 1]))
+
+                start = nums[i]
+
+        if start == nums[-1]:
+            result.append(str(start))
+        else:
+            result.append(str(start) + "->" + str(nums[-1]))
+
+        return result
+
+if __name__ == "__main__":
+    sol = Solution()
+    nums = [0, 1, 2, 4, 5, 7]
+    print(sol.summaryRanges(nums))
