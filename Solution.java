6dfba8bf64d08cdc33db4cf027fe77250d8eff4a
AyushSolves <ayushnegi196@gmail.com>
2026-03-01 10:43:09 +0530

    Add files via upload

--- Solution.java
+++ Solution.java
@@ -1,57 +1,48 @@
-class ListNode 
-{
-    int val;
-    ListNode next;
-    ListNode(int val) { this.val = val; }
-}
+import java.util.*;
 
-class Solution 
-{
+public class Solution {
 
-    public boolean isPalindrome(ListNode head) 
-    {
-
-        if (head == null || head.next == null) 
-        {
-            return true;
-        }
-
-        ListNode slow = head;
-        ListNode fast = head;
-
-        while (fast != null && fast.next != null) 
-        {
-            slow = slow.next;
-            fast = fast.next.next;
-        }
-
-        ListNode secondHalf = reverse(slow);
-
-        ListNode firstHalf = head;
-        while (secondHalf != null) 
-        {
-            if (firstHalf.val != secondHalf.val) 
-            {
-                return false;
-            }
-            firstHalf = firstHalf.next;
-            secondHalf = secondHalf.next;
-        }
-
-        return true;
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int val) { this.val = val; }
     }
 
-    private ListNode reverse(ListNode head) 
-    {
-        ListNode prev = null;
-
-        while (head != null) {
-            ListNode nextNode = head.next;
-            head.next = prev;
-            prev = head;
-            head = nextNode;
+    public List<String> binaryTreePaths(TreeNode root) {
+        List<String> result = new ArrayList<>();
+        
+        if (root == null) {
+            return result;
         }
 
-        return prev;
+        dfs(root, "", result);
+        return result;
+    }
+
+    private void dfs(TreeNode node, String path, List<String> result) {
+
+        if (node == null) return;
+
+        if (node.left == null && node.right == null) {
+            result.add(path + node.val);
+            return;
+        }
+
+        dfs(node.left, path + node.val + "->", result);
+        dfs(node.right, path + node.val + "->", result);
+    }
+
+    public static void main(String[] args) 
+    {
+
+        Solution sol = new Solution();
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(3);
+        root.left.right = new TreeNode(5);
+
+        List<String> result = sol.binaryTreePaths(root);
+        System.out.println(result);
     }
 }
