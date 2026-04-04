5df2f61079b7f66f94d60c1ba0660c846dbe5b5f
AyushSolves <ayushnegi196@gmail.com>
2026-04-04 17:19:44 +0530

    Add files via upload

--- Solution.java
+++ Solution.java
@@ -1,41 +1,33 @@
-
 public class Solution {
 
-    public String addStrings(String num1, String num2) {
+    public int arrangeCoins(int n) {
 
-        StringBuilder result = new StringBuilder();
+        long left = 0;
+        long right = n;
 
-        int i = num1.length() - 1;
-        int j = num2.length() - 1;
-        int carry = 0;
+        while (left <= right) {
+            long mid = left + (right - left) / 2;
 
-        while (i >= 0 || j >= 0 || carry > 0) {
+            long coins = mid * (mid + 1) / 2;
 
-            int digit1 = (i >= 0) ? num1.charAt(i) - '0' : 0;
-            int digit2 = (j >= 0) ? num2.charAt(j) - '0' : 0;
-
-            int sum = digit1 + digit2 + carry;
-
-            result.append(sum % 10);
-            carry = sum / 10;
-
-            i--;
-            j--;
+            if (coins == n) {
+                return (int) mid;
+            } else if (coins < n) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
         }
 
-        return result.reverse().toString();
+        return (int) right;
     }
 
+    // For VS Code testing
     public static void main(String[] args) {
 
         Solution sol = new Solution();
 
-        String num1 = "11";
-        String num2 = "123";
-
-        System.out.println(sol.addStrings(num1, num2)); // 134
-
-        System.out.println(sol.addStrings("456", "77")); // 533
-        System.out.println(sol.addStrings("0", "0"));   // 0
+        System.out.println(sol.arrangeCoins(5));  // 2
+        System.out.println(sol.arrangeCoins(8));  // 3
     }
 }
