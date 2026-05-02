df5d4b34f3dfce96737b99e6e244233d6a7c0db3
AyushSolves <ayushnegi196@gmail.com>
2026-05-02 22:56:24 +0530

    Add files via upload

---
+++ perfectnumber.py
@@ -0,0 +1,21 @@
+class Solution:
+    def checkPerfectNumber(self, num: int) -> bool:
+        if num <= 1:
+            return False
+
+        total = 1  # 1 is always a divisor
+
+        i = 2
+        while i * i <= num:
+            if num % i == 0:
+                total += i
+                if i != num // i: 
+                    total += num // i
+            i += 1
+
+        return total == num
+
+if __name__ == "__main__":
+    sol = Solution()
+    print(sol.checkPerfectNumber(28)) 
+    print(sol.checkPerfectNumber(7))   
